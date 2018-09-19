e010903357dfc07b45f3c64b0ee0e6526fdc30cf
lhrbueno <luishenriquebueno@gmail.com>
2018-09-18 23:44:37 -0300

    Added contructor accepting only message

--- src/main/java/com/home/workshopmongo/exceptions/UserNotFoundException.java
+++ src/main/java/com/home/workshopmongo/exceptions/UserNotFoundException.java
@@ -4,6 +4,10 @@ public class UserNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
+	public UserNotFoundException(String msg) {
+		super(msg);
+	}
+	
 	public UserNotFoundException(String msg, Throwable e) {
 		super(msg, e);
 	}
