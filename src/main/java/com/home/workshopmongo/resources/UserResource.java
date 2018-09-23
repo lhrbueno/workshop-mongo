97dfb87bfc82e02dd127093cb9886b79ab574e0b
lhrbueno <luishenriquebueno@gmail.com>
2018-09-23 00:57:57 -0300

    Removed unnecessary code

--- src/main/java/com/home/workshopmongo/resources/UserResource.java
+++ src/main/java/com/home/workshopmongo/resources/UserResource.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,67 +28,29 @@ import com.home.workshopmongo.utils.MessageProperties;
 @RequestMapping("/users")
 public class UserResource {
 
-	private static Logger LOGGER = LogManager.getLogger(UserResource.class);
-	
 	@Autowired
 	private UserService userService;
-	
-	@Autowired
-	private MessageProperties messageProperties;
 	
 	@GetMapping
 	@ResponseBody
 	public ResponseEntity<?> findAll() {
-		
-		try {
-			
-			List<User> users = userService.findAll();
-			List<UserDTO> usersDTO = users.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
-			LOGGER.info(messageProperties.getMessage("user.all.success"));
-			return new ResponseEntity<List<UserDTO>>(usersDTO, HttpStatus.OK);
-			
-		} catch (UserNotFoundException e) {
-			
-			LOGGER.info(e.getMessage());
-			Map<String, String> response = new HashMap<>();
-			response.put("message", e.getMessage());
-			return new ResponseEntity<Map<String, String>>(response, HttpStatus.ACCEPTED);
-			
-		} catch (Exception e) {
-			
-			LOGGER.info(e.getMessage(), e);
-			Map<String, String> response = new HashMap<>();
-			response.put("message", messageProperties.getMessage("server.status.500"));
-			return new ResponseEntity<Map<String, String>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-			
-		}
-		
+		List<User> users = userService.findAll();
+		List<UserDTO> usersDTO = users.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
+		return new ResponseEntity<List<UserDTO>>(usersDTO, HttpStatus.OK);
 	}
 	
 	@GetMapping("{id}")
 	@ResponseBody
 	public ResponseEntity<?> findOne(@PathVariable("id") String id) {
-		
-		try {
-			User user = userService.findById(id);
-			LOGGER.info(messageProperties.getMessage("user.single.success"));
-			return new ResponseEntity<UserDTO>(new UserDTO(user), HttpStatus.OK);
-			
-		} catch (UserNotFoundException e) {
-			
-			LOGGER.info(e.getMessage());
-			Map<String, String> response = new HashMap<>();
-			response.put("message", e.getMessage());
-			return new ResponseEntity<Map<String, String>>(response, HttpStatus.ACCEPTED);
-			
-		} catch (Exception e) {
-			
-			LOGGER.info(e.getMessage(), e);
-			Map<String, String> response = new HashMap<>();
-			response.put("message", messageProperties.getMessage("server.status.500"));
-			return new ResponseEntity<Map<String, String>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-			
-		}
+		User user = userService.findById(id);
+		return new ResponseEntity<UserDTO>(new UserDTO(user), HttpStatus.OK);
+	}
+	
+	@PutMapping
+	@ResponseBody
+	public ResponseEntity<?> updateUser(@PathVariable String id,  @RequestBody User user) {
+		User userExists = userService.findById(id);
+		return null;
 	}
 	
 }
