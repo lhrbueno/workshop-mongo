d533e157888e2e9f04c161d30a5a996b3e20202b
lhrbueno <luishenriquebueno@gmail.com>
2018-09-23 00:56:01 -0300

    Added exception handler and custom message class

---
+++ src/main/java/com/home/workshopmongo/resources/exceptions/MessageResponse.java
@@ -0,0 +1,36 @@
+package com.home.workshopmongo.resources.exceptions;
+
+import java.io.Serializable;
+
+public class MessageResponse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int status;
+	
+	private String message;
+	
+	public MessageResponse() {}
+
+	public MessageResponse(int status, String message) {
+		this.status = status;
+		this.message = message;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
---
+++ src/main/java/com/home/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.home.workshopmongo.resources.exceptions;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.home.workshopmongo.exceptions.UserNotFoundException;
+import com.home.workshopmongo.utils.MessageProperties;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	private static Logger LOGGER = LogManager.getLogger(ResourceExceptionHandler.class);
+	
+	@Autowired
+	private MessageProperties messageProperties;
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<MessageResponse> userNotFound(UserNotFoundException e) {
+		LOGGER.info(e.getMessage());
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		MessageResponse mr = new MessageResponse(status.value(), e.getMessage());
+		return ResponseEntity.status(status).body(mr);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<MessageResponse> genericError(Exception e) {
+		LOGGER.info(e.getMessage());
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		MessageResponse mr = new MessageResponse(status.value(), messageProperties.getMessage("server.status.500"));
+		return ResponseEntity.status(status).body(mr);
+	}
+	
+}
